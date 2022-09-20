efdaeeb87ebe9e454fc67366ac100b839efc8033
evansciver1 <112420646+evansciver1@users.noreply.github.com>
2022-09-20 10:21:06 -0400

    Update Notes.sh

--- Notes.sh
+++ Notes.sh
@@ -1,3 +1,12 @@
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
 Day 1
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Powershell:
+  - CIM classes are general collections of objects which fall under specific criteria; pull things directly from the kernel
+  - CIM classes are a form of persistence; Win32 is exclusively microsoft
+  - 'get-wmiobject win32_process | select -first 2 -erroraction silentlycontinue' syntax to take first 2 fields from the output of a get win32 command and ignore error messages
+  - Remote sign allows you to run any script as long as it isnt on the local machine
+  - get-executionpolicy shows execution policy; powershell reads policy before opening profiles
+  - $profile.allusersallhosts applies to everyone; persistence using this looks for highest precdent
+  - ($profile | get-member -membertype noteproperty).name | foreach-object { if ((test-path $PROFILE.$_) -eq $true) { write-host "$PROFILE.$_" -foregroundcolor green -nonewline ; write-host "exists }}
+    getss user profiles and confirms they exist
