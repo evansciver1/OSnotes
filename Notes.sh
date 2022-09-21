00f3574535158b6aa6f0f9ef8e75ab00fceb590f
evansciver1 <112420646+evansciver1@users.noreply.github.com>
2022-09-21 10:35:51 -0400

    Update Notes.sh

--- Notes.sh
+++ Notes.sh
@@ -8,12 +8,25 @@ Powershell:
   - Remote sign allows you to run any script as long as it isnt on the local machine
   - get-executionpolicy shows execution policy; powershell reads policy before opening profiles
   - $profile.allusersallhosts applies to everyone; persistence using this looks for highest precdent
-  - ($profile | get-member -membertype noteproperty).name | foreach-object { if ((test-path $PROFILE.$_) -eq $true) { write-host "$PROFILE.$_" -foregroundcolor green -nonewline ; write-host "exists }}
+  - '($profile | get-member -membertype noteproperty).name | foreach-object { if ((test-path $PROFILE.$_) -eq $true) { write-host "$PROFILE.$_" -foregroundcolor green -nonewline ; write-host "exists" }}'
     getss user profiles and confirms they exist
   - Remoting:
     - requires windows remote management to be running [winrm]
-    - get-item wsman:\localhost\client\trustedhosts shows what computers are trusted to remote in; by default only allows one addition
+    - 'get-item wsman:\localhost\client\trustedhosts' shows what computers are trusted to remote in; by default only allows one addition
     - using quotes around what you want to add w/get-item allows multiple additions i.e. ... -value "10.20.30.40,not_kremlin.ru" will add both
     - 'invoke-command' also works to temporarily remote in i.e. 'Invoke-Command -ComputerName File-Server {Get-Service}'
     - using -asjob while querying a large number of hosts or data requires the recieve-job command to be run after 
-    
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Day 2
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------    
+ Registry Structure:
+  - 'Reg.exe, Regedit (registry editor)' tools to edit the registry
+  - 'HKLM\software\microsoft\windows\CurrentVersion\run' - anything in this key is run whenever the system is started/restarted
+  - Hive keys/HKs - HKLM/HKEY_USERS/HKEY_CURRENT_USERS/HKEY_CURRENT_CONFIG/HKEY_CLASSES_ROOT
+  - Reg.exe will treat everthing as the absolute path; using tab, *, etc. doesn't work because reg believes that's the actual path
+  - get-item will query individual keys; get-childitem will get all keys under a certain path i.e. all keys under HKLM\Software
+  - get-itemproperty gives values and properties of individual registry entry
+ Alternate Data Streams:
+  - Only exist in NTFS drive; cannot be disabled
+  - '[filename.extension]:[alternate_stream_name]:$DATA'
+  - 
